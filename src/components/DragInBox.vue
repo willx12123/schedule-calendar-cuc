3a029ecb003d9855a28b393c3d73e712ecd35f8a
AerospaceXu <hangyu_xu@foxmail.com>
2020-10-26 19:23:45 +0800

    feat: Create DragInBox.

---
+++ src/components/DragInBox.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="drag-in-wrapper"
+    @dragover.prevent.stop
+    @dragenter.prevent.stop
+    @drop.prevent.stop="handleFileDrop"
+  >
+    拖拽到此处
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'DragInBox',
+  setup() {
+    const handleFileDrop = () => {
+      console.log('拖入了');
+    };
+    return {
+      handleFileDrop,
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.drag-in-wrapper {
+  border: 1px solid black;
+  width: 400px;
+  height: 300px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,29 @@
 <template>
-  <div class="home"></div>
+  <div class="home-wrapper">
+    <drag-in-box />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
+import DragInBox from '@/components/DragInBox.vue';
+
 export default defineComponent({
   name: 'Home',
+  components: {
+    DragInBox,
+  },
   setup() {
     return {};
   },
 });
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.home-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
