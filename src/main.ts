be8df4794d3d2a401c6e39c939ae11b390ee5fe2
AerospaceXu <hangyu_xu@foxmail.com>
2020-10-26 18:52:48 +0800

    feat: Init project, add dependencies.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { createApp } from 'vue';
-import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
+
+import App from './App.vue';
+import './style.scss';
 
 createApp(App)
   .use(router)
