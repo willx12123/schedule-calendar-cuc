45e517454e71eb1c21162135c68357b6f2b438ad
AerospaceXu <hangyu_xu@foxmail.com>
2020-10-27 11:00:08 +0800

    feat: Use useWindow hook to add style.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,65 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
-    |
-    <router-link to="/help">Help</router-link>
+  <div class="app-wrapper" :style="appStyle">
+    <nav>
+      <router-link to="/">Home</router-link>
+      |
+      <router-link to="/about">About</router-link>
+      |
+      <router-link to="/help">Help</router-link>
+    </nav>
+    <main>
+      <router-view />
+    </main>
+    <footer></footer>
   </div>
-  <router-view />
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed } from 'vue';
+import { useWindow } from '@/hooks/useWindow';
 
 export default defineComponent({
   name: 'App',
   setup() {
-    return {};
+    const { clientSize } = useWindow();
+    const appStyle = computed(() => {
+      const { width, height } = clientSize;
+      return {
+        width: `${width}px`,
+        height: `${height}px`,
+      };
+    });
+    return {
+      appStyle,
+    };
   },
 });
 </script>
 
-<style lang="scss"></style>
+<style scoped lang="scss">
+.app-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > nav {
+    width: 100%;
+    padding: 8px 0;
+    display: flex;
+    justify-content: center;
+  }
+
+  > main {
+    padding-bottom: 40px;
+    flex-grow: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  > footer {
+    height: 160px;
+    width: 100%;
+  }
+}
+</style>
