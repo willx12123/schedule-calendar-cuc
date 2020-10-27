ca1291b2460292de5f9a741169bd3e8d1dfe0c53
AerospaceXu <hangyu_xu@foxmail.com>
2020-10-27 11:05:27 +0800

    feat: run vue lint to fix files` end-of-line.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{ts,vue}', '!**/node_modules/**'],
   transform: {
     '^.+\\.vue$': 'vue-jest',
   },
