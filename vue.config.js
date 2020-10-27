79f091d13bc1ed4cd9ce1f83410542ef0152a7a8
AerospaceXu <hangyu_xu@foxmail.com>
2020-10-27 16:39:36 +0800

    chore: Update vue config.

---
+++ vue.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/schedule-calendar-cuc/' : './',
+  productionSourceMap: false,
+  pwa: {
+    manifestOptions: {
+      icons: [],
+    },
+    iconPaths: {
+      favicon32: './favicon.ico',
+      favicon16: './favicon.ico',
+      appleTouchIcon: './favicon.ico',
+      maskIcon: './favicon.ico',
+      msTileImage: './favicon.ico',
+    },
+  },
+};
