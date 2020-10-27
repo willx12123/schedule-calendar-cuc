2a07f15899992754441236c81ea6a4960e5c0283
AerospaceXu <hangyu_xu@foxmail.com>
2020-10-27 17:13:43 +0800

    feat: Fix path.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/schedule-calendar-cuc/' : './',
+    process.env.NODE_ENV === 'production' ? '/schedule-calendar-cuc/dist/' : './',
   productionSourceMap: false,
   pwa: {
     manifestOptions: {
